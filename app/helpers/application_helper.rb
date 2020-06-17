74e89feca7b97d191c65d209552a08e16a4146cd
Evaldo Kalbermatter <ekalber@gmail.com>
2020-06-17 14:55:18 -0300

    17/06

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,8 +15,15 @@ module ApplicationHelper
     if product.stock < 1
       return link_to 'NO STOCK', "#", class: 'btn btn-sm btn-outline-danger'
     else
+      
       return link_to 'BUY', homes_path, class: 'btn btn-sm btn-outline-success'
+      
+    end
+  end
 
+  def check_stockk(product)
+    if product.stock > 1
+      return link_to 'Add to Cart', line_items_path(product_id: product), class: 'btn btn-sm btn-outline-secondary'
     end
   end
 
